bf7f1bec68db03af36ee500c3feac0aa9f05c0e6
VoidBlanc <angelic.noted_0g@icloud.com>
2023-07-25 17:08:35 +0100

    Change code to use nextjs now testing phase to test the deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    assetPrefix: '/WilliamsCollection/',
+};
+
+module.exports = nextConfig
