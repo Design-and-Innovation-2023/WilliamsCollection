4bd29af5d9299e79092f28c93cf7a986d8985da9
VoidBlanc <angelic.noted_0g@icloud.com>
2023-08-03 10:31:31 +0100

    Updated some heading

--- src/app/past-journey/emailupdates/page.tsx
+++ src/app/past-journey/emailupdates/page.tsx
@@ -6,8 +6,8 @@ export default function EmailUpdates() {
         <main className="mb-auto">
           <div className="divide-y divide-gray-200 dark:divide-gray-700">
             <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-              <h1 className="text-white text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-                Daily updates to our client.
+              <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-amber-600 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                Daily Client Updates
               </h1>
               <p className="text-lg leading-7 text-white">
                 Team 3A Daily Emails
