7d9507d886dfd168078638edc335e47860b49663
VoidBlanc <angelic.noted_0g@icloud.com>
2023-08-04 12:39:53 +0100

    Added credits page

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@material-tailwind/react";
-
+import Link from "next/link";
 export function SimpleFooter() {
   return (
     <footer className="flex w-screen flex-row flex-wrap mx-auto px-10 items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
@@ -8,14 +8,15 @@ export function SimpleFooter() {
       </Typography>
       <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
         <li>
-          <Typography
-            as="a"
-            href="#"
-            color="amber"
-            className="font-normal transition-colors amber-500"
-          >
-            Credits
-          </Typography>
+          <Link href="/credits">
+            <Typography
+              as="a"
+              color="amber"
+              className="font-normal transition-colors amber-500"
+            >
+              Credits
+            </Typography>
+          </Link>
         </li>
       </ul>
     </footer>
---
+++ src/app/credits/page.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+export default function CreditsPage() {
+  return (
+    <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <div className="flex h-screen flex-col justify-between">
+        <main className="mb-auto">
+          <div className="divide-y divide-gray-200 dark:divide-gray-700">
+            <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+              <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-amber-600 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                Credits
+              </h1>
+            </div>
+            <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+              <li className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-white">
+                        <p>Device Frame</p>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div className="prose max-w-none text-white text-justify dark:text-white">
+                          <div className="flex">
+                            <div
+                              className="team-list text-amber-500"
+                              style={{ marginRight: "50px" }}
+                            >
+                              <p>
+                                Device Mockup created from{" "}
+                                <Link href="https://deviceframes.com/templates/iphone-14">
+                                  Iphone mockups
+                                </Link>
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            </ul>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
